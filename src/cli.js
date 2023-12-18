7cccb8b85677761a36cae23d15ec6ac91e7505d5
Maria Berina <gia0sam8cint@bk.ru>
2023-12-18 17:20:56 +0300

    Install readline-sync, add brain-games.js, create src/cli.js
    	new file:   src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import  readlineSync  from 'readline-sync';
+
+export default () => {
+	var userName = readlineSync.question('May I have your name? ');
+	console.log('Hello, ' + userName + '!');
+};
