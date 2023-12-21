eeb2a60972510959a32d8e71f02a60eec0b3bb47
Maria Berina <gia0sam8cint@bk.ru>
2023-12-21 20:08:57 +0300

    Finish bin/brain-even.js, add package.json, Makefile, README.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/miley777/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/miley777/frontend-project-44/actions)
 
 <a href="https://codeclimate.com/github/miley777/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/0324022578cfd58048c8/maintainability" /></a>
+
+https://asciinema.org/a/g1i3e5q83XIK8Z6p5F6uftbW7
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,18 +1,22 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import getName from '../src/cli.js';
 
 const checkAnswer = () => {
   const systNum = Math.floor(Math.random() * 10);
   console.log(`Question: ${systNum}`);
   const usersAnswer = readlineSync.question('Your answer: ');
   const systemAnswer = systNum % 2 === 0 ? 'yes' : 'no';
+  let desicion = 0;
   if (systemAnswer !== usersAnswer) {
-    return 1;
+    desicion = usersAnswer === 'yes' ? 1 : -1;
   }
-  return 0;
+  return desicion;
 };
 
 const checkParity = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   let count = 0;
@@ -23,15 +27,19 @@ const checkParity = () => {
     if (answer === 0) {
       count += 1;
       console.log('Correct!');
-    } else {
-      console.log(`Let's try again, ${getName.userName}`);
+    } else if (answer !== 0) {
+      if (answer === 1) {
+        console.log("'yes' is wrong ;(. Correct answer was 'no'");
+      } else if (answer === -1) {
+        console.log("'no' is wrong ;(. Correct answer was 'yes'");
+      }
+      console.log(`Let's try again, ${userName}`);
       break;
     }
   }
   if (count === 3) {
-    console.log(`Congratulations, ${getName.userName}`);
+    console.log(`Congratulations, ${userName}`);
   }
 };
 
-getName();
 checkParity();
