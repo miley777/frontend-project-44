5160f08cb070cae73d6bedebad60ccd92faeb80e
Maria Berina <gia0sam8cint@bk.ru>
2023-12-25 21:52:34 +0300

    Fix bin/games/brain-gcd.js

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -11,8 +11,8 @@ const getArrDiv = (systNum1, systNum2) => {
   const arrDiv = [];
   let count = 0;
   arrDiv.push(count);
-  const minSystNum = Math.max(systNum1, systNum2);
-  while (count <= minSystNum) {
+  const maxSystNum = Math.max(systNum1, systNum2);
+  while (count <= maxSystNum) {
     if ((systNum1 % count === 0) && (systNum2 % count === 0)) {
       arrDiv.push(count);
     }
@@ -53,56 +53,3 @@ const checkGcd = () => {
 };
 
 checkGcd();
-
-/*
-import readlineSync from 'readline-sync';
-// eslint-disable-next-line import/named, import/no-cycle
-import { questionGreeting, randomNum, userName } from '../../src/index.js';
-
-const checkAnswer = () => {
-  const systNum1 = randomNum();
-  const systNum2 = randomNum();
-  console.log(`Question: ${systNum1} ${systNum2}`);
-  const usersAnswer = readlineSync.question('Your answer: ');
-  const arrDiv = [];
-  let count = 0;
-  arrDiv.push(count);
-  const minSystNum = Math.min(systNum1, systNum2);
-  while (count <= minSystNum) {
-    if ((systNum1 % count === 0) && (systNum2 % count === 0)) {
-      arrDiv.push(count);
-    }
-    count += 1;
-  }
-  const result = arrDiv[arrDiv.length - 1];
-  const desicion = result === Number(usersAnswer) ? 0 : 1;
-  if (desicion !== 0) {
-    console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${result}'`);
-  }
-  return desicion;
-};
-
-const checkGcd = () => {
-  questionGreeting();
-  console.log('Find the greatest common divisor of given numbers.');
-  let count = 0;
-  const countCorrect = 3;
-  const nameOfUser = userName;
-  for (let i = 0; i < countCorrect; i += 1) {
-    const answer = checkAnswer();
-    if (answer === 0) {
-      count += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`Let's try again, ${nameOfUser}`);
-      break;
-    }
-  }
-  if (count === 3) {
-    console.log(`Congratulations, ${nameOfUser}`);
-  }
-};
-
-checkGcd();
-
-*/
